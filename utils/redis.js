ffd6272287022a0b32e6b432b15f3509f43a19b9
Clifford Mapesa <cliffordmapesa8@gmail.com>
2024-06-28 12:09:12 +0300

    feat: redis utils

---
+++ utils/redis.js
@@ -0,0 +1,35 @@
+// class RedisClient
+import { createClient } from 'redis';
+import { promisify } from 'util';
+
+class RedisClient {
+  constructor() {
+    this.client = createClient();
+
+    this.client.on('error', (err) => {
+      console.error('Redis client error', err);
+    });
+  }
+
+  isAlive() {
+    return this.client.connected;
+  }
+
+  async get(key) {
+    const getAsync = promisify(this.client.get).bind(this.client);
+    return getAsync(key);
+  }
+
+  async set(key, value, duration) {
+    const setAsync = promisify(this.client.setex).bind(this.client);
+    return setAsync(key, duration, value);
+  }
+
+  async del(key) {
+    const delAsync = promisify(this.client.del).bind(this.client);
+    await delAsync(key);
+  }
+}
+
+const redisClient = new RedisClient();
+export default redisClient;
