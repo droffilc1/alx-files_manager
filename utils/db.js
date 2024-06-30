535b2d64c4c6cb780147d6dabfc61ab03731694d
Clifford Mapesa <cliffordmapesa8@gmail.com>
2024-06-30 11:14:47 +0300

    fix: MongoDB utils

--- utils/db.js
+++ utils/db.js
@@ -1,37 +1,33 @@
 // class DBClient
 import { MongoClient } from 'mongodb';
 
-const DB_HOST = process.env.DB_HOST || 'localhost';
-const DB_PORT = process.env.DB_PORT || 27017;
-const DB_DATABASE = process.env.DB_DATABASE || 'file_manager';
-const url = `mongodb://${DB_HOST}:${DB_PORT}`;
-
 class DBClient {
   constructor() {
-    const options = { useUnifiedTopology: true };
-    MongoClient.connect(url, options, (error, client) => {
-      if (error) {
-        console.log(error.message);
-        this.db = false;
-        return;
-      }
-      this.db = client.db(DB_DATABASE);
-      this.users = this.db.collection('users');
-      this.files = this.db.collection('files');
-    });
+    const DB_HOST = process.env.DB_HOST || 'localhost';
+    const DB_PORT = process.env.DB_PORT || 27017;
+    const DB_DATABASE = process.env.DB_DATABASE || 'file_manager';
+    const url = `mongodb://${DB_HOST}:${DB_PORT}`;
+
+    this.client = new MongoClient(url, { useUnifiedTopology: true });
+    this.client.connect()
+      .then(() => {
+        this.db = this.client.db(DB_DATABASE);
+      })
+      .catch((err) => {
+        console.error(err.message);
+      });
   }
 
   isAlive() {
-    // https://stackoverflow.com/questions/784929/what-is-the-not-not-operator-in-javascript
     return !!this.db;
   }
 
   async nbUsers() {
-    return this.users.countDocuments();
+    return this.db.collection('users').countDocuments();
   }
 
   async nbFiles() {
-    return this.files.countDocuments();
+    return this.db.collection('files').countDocuments();
   }
 }
 
