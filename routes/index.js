4ec2fd45209d0d50fcdab14f938f819bdb76382f
Clifford Mapesa <cliffordmapesa8@gmail.com>
2024-06-30 12:29:33 +0300

    fix: first API

--- routes/index.js
+++ routes/index.js
@@ -1,26 +1,40 @@
-import { Router } from 'express';
+import express from 'express';
 import AppController from '../controllers/AppController';
 import AuthController from '../controllers/AuthController';
 import UsersController from '../controllers/UsersController';
 
-const router = Router();
+export default function Routes(app) {
+  const router = express.Router();
+  router.use(express.json());
+  app.use('/', router);
 
-// GET /status => AppController.getStatus
-router.get('/status', AppController.getStatus);
+  // GET /status => AppController.getStatus
+  router.get('/status', (req, res) => {
+    AppController.getStatus(req, res);
+  });
 
-// GET /stats => AppController.getStats
-router.get('/stats', AppController.getStats);
+  // GET /stats => AppController.getStats
+  router.get('/stats', (req, res) => {
+    AppController.getStats(req, res);
+  });
 
-// POST /users => UsersController.postNew
-router.post('/users', UsersController.postNew);
+  // POST /users => UsersController.postNew
+  router.post('/users', (req, res) => {
+    UsersController.postNew(req, res);
+  });
 
-// GET /users/me => UserController.getMe
-router.get('/users/me', UsersController.getMe);
+  // GET /users/me => UserController.getMe
+  router.get('/users/me', (req, res) => {
+    UsersController.getMe(req, res);
+  });
 
-// GET /connect => AuthController.getConnect
-router.get('/connect', AuthController.getConnect);
+  // GET /connect => AuthController.getConnect
+  router.get('/connect', (req, res) => {
+    AuthController.getConnect(req, res);
+  });
 
-// GET /disconnect => AuthController.getDisconnect
-router.get('/disconnect', AuthController.getDisconnect);
-
-export default router;
+  // GET /disconnect => AuthController.getDisconnect
+  router.get('/disconnect', (req, res) => {
+    AuthController.getDisconnect(res, res);
+  });
+}
--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 import express from 'express';
-import router from './routes/index';
+import Routes from './routes/index';
 
 // Set the network port
 const PORT = process.env.PORT || 5000;
 // Init the Express application
 const app = express();
 
-app.use('/', router);
+// Routes
+Routes(app);
+app.use('/', Routes);
 
 // Start the server
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running http://localhost:${PORT}`);
+  console.log('Press CTRL+C to stop server');
 });
-
-export default app;
