a69f5462c21665af01a1bd5582eba837f5df5dd6
Clifford Mapesa <cliffordmapesa8@gmail.com>
2024-06-30 17:19:46 +0300

    feat: authenticate a user

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,5 +1,7 @@
+import { ObjectId } from 'mongodb';
 import sha1 from 'sha1';
 import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
 
 export default class UsersController {
   static async postNew(req, res) {
@@ -44,10 +46,22 @@ export default class UsersController {
   }
 
   static async getMe(req, res) {
-    const { user } = req;
-
-    if (!user) {
-      res.status(200).json({ email: user.email, id: user._id.toString() });
+    const token = req.headers['x-token'];
+    if (!token) {
+      return res.status(401).json({ error: 'Unauthorized' });
     }
+
+    const key = `auth_${token}`;
+    const userId = await redisClient.get(key);
+    if (!userId) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const user = await dbClient.db.collection('users').findOne({ _id: new ObjectId(userId) });
+    if (!user) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    return res.status(200).json({ email: user.email, id: user._id });
   }
 }
